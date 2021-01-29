8962cf485d9fe495168ae7fe498d4f6beddfb6a4
Thyago Vinicius Camilo Cunha <thyago.vccunha@gmail.com>
2021-01-28 21:08:10 -0300

    feat: adicionar movimento básico ao jogador

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float Speed = 1.0f;
+    
+    void Update()
+    {
+        Move(GetDirection());
+    }
+
+    private void Move(Vector3 direction)
+    {
+        transform.position += Time.deltaTime * Speed * direction;
+    }
+
+    private Vector3 GetDirection()
+    {
+        Vector3 direction = Vector3.zero;
+        if (Input.GetKey(KeyCode.W)) direction += Vector3.up;
+        if (Input.GetKey(KeyCode.S)) direction += Vector3.down;
+        if (Input.GetKey(KeyCode.A)) direction += Vector3.left;
+        if (Input.GetKey(KeyCode.D)) direction += Vector3.right;
+
+        return direction;
+    }
+}
